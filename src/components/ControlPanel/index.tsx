1eaf6ea1c7676215c5d591bf8e0cee40da2667d0
Vitaliyeb <vlvlv.vovov@bk.ru>
2021-12-16 18:13:07 +0500

    panel

--- src/components/ControlPanel/index.tsx
+++ src/components/ControlPanel/index.tsx
@@ -1,9 +1,22 @@
 import styles from './style.module.css';
+import {useState} from "react";
+import classNames from "classnames";
 
 const ControlPanel = () => {
+    const [isShow, setIsShow] = useState(true);
 
-    return <div className={styles.panelWrapper}>
+    return <div className={classNames(styles.panelWrapper, {[styles.panelShow]: isShow})}>
+        <div className={styles.actionsBar}>
+            <button>Run</button>
+            <button>Input parameters</button>
+            <button
+                onClick={() => setIsShow(!isShow)}
+                className={classNames(styles.viewsButton, {[styles.viewsButtonShow]: isShow})}
+            />
+        </div>
+        <div className={classNames(styles.content)}>
 
+        </div>
     </div>
 }
 
