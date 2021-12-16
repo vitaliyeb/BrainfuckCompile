49adf8d25a7b8ffaaef516758b83cb1243f68e76
Vitaliyeb <vlvlv.vovov@bk.ru>
2021-12-16 16:03:43 +0500

    add editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Editor from "./components/Editor";
+
 
 function App() {
-  return (
-    <div className="App">
-      hello app
-    </div>
-  );
+    const [value, setValue] = useState('')
+
+    return (
+      <div className="App">
+          <Editor
+              value={value}
+              setValue={setValue}
+          />
+      </div>
+    );
 }
 
 export default App;
