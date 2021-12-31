12d938675b3758900344a5907061fde225607504
Vitaliy Burdin <vlvlv.vovov@bk.ru>
2021-12-31 19:31:50 +0500

    сдщыу игеещт

--- src/components/QuestionModal/index.tsx
+++ src/components/QuestionModal/index.tsx
@@ -3,7 +3,7 @@ import styles from './style.module.css';
 const QuestionModal = () => {
     return <div className={styles.wrapper}>
         <div className={styles.modal}>
-
+            <div className={styles.close}></div>
         </div>
     </div>
 };
